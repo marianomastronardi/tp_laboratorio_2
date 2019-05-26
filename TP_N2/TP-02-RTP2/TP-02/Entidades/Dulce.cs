1f6e3a5df6f523a8413593e4c3f6c451bbca4c8e
Mariano <marianogarciamastronardi@yahoo.com.ar>
2019-05-26 12:20:47 -0300

    Recuperatorio 2019 - correccion TP2 desaprobado

---
+++ TP_N2/TP-02-RTP2/TP-02/Entidades/Dulce.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text;
+
+namespace Entidades_2018
+{
+    public class Dulce : Producto
+    {
+        #region "Constructores"
+        public Dulce(EMarca marca, string patente, ConsoleColor color) : base(patente, marca, color)
+        {
+        }
+        #endregion
+
+        #region "Propiedades"
+        /// <summary>
+        /// Los dulces tienen 80 calorías
+        /// </summary>
+        protected override short CantidadCalorias
+        {
+            get
+            {
+                return 80;
+            }
+        }
+        #endregion
+
+        #region "Métodos"
+        /// <summary>
+        /// Publica todos los datos de Dulce y su clase padre Producto
+        /// </summary>
+        /// <returns>string con los valores asociados a la instancia de la clase</returns>
+        public override string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("DULCE");
+            sb.AppendLine((string)this);
+            sb.Append(string.Format("CALORIAS : {0}", this.CantidadCalorias));
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+        #endregion
+    }
+}
---
+++ TP_N2/TP-02-RTP2/TP-02/Entidades/Snacks.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text;
+
+namespace Entidades_2018
+{
+    public class Snacks : Producto
+    {
+        #region "Constructores"
+        public Snacks(EMarca marca, string patente, ConsoleColor color) : base(patente, marca, color)
+        {
+        }
+        #endregion
+
+        #region "Propiesdades"
+        /// <summary>
+        /// Los snacks tienen 104 calorías
+        /// </summary>
+        protected override short CantidadCalorias
+        {
+            get
+            {
+                return 104;
+            }
+        }
+        #endregion
+
+        #region "Métodos"
+        /// <summary>
+        /// Muestra llos atributos de Snacks
+        /// </summary>
+        /// <returns>retorna los atributos de Snacks y su clase padre</returns>
+        public override string Mostrar()
+        {
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("SNACKS");
+            sb.AppendLine((string)this);
+            sb.AppendLine(string.Format("CALORIAS : {0}", this.CantidadCalorias));
+            sb.AppendLine("----------------------");
+
+            return sb.ToString();
+        }
+        #endregion
+    }
+}
