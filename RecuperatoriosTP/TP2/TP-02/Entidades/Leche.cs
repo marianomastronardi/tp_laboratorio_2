877fbb41835a101f2d32153e7a536de200c3e6b5
Mariano <marianogarciamastronardi@yahoo.com.ar>
2019-07-18 23:42:14 -0300

    recupertorios tp

---
+++ RecuperatoriosTP/TP2/TP-02/Entidades/Leche.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Text;
+
+namespace Entidades_2018
+{
+    public class Leche : Producto
+    {
+
+        #region "Atributos"
+        public enum ETipo { Entera, Descremada }
+        private ETipo tipo;
+        #endregion
+
+        #region "Constructores"
+        /// <summary>
+        /// Por defecto, TIPO será ENTERA
+        /// </summary>
+        /// <param name="marca"></param>
+        /// <param name="patente"></param>
+        /// <param name="color"></param>
+        public Leche(EMarca marca, string patente, ConsoleColor color) : this(marca, patente, color, ETipo.Entera)
+        {
+
+        }
+
+        public Leche(EMarca marca, string patente, ConsoleColor color, ETipo tipo) : base(patente, marca, color)
+        {
+            this.tipo = tipo;
+        }
+        #endregion
+
+        #region "Propiedades"
+        /// <summary>
+        /// Las leches tienen 20 calorías
+        /// </summary>
+        protected override short CantidadCalorias
+        {
+            get
+            {
+                return 20;
+            }
+        }
+        #endregion
+
+        #region "Métodos"
+        /// <summary>
+        /// Publica todos los datos de Leche y los de su clase padre Producto
+        /// </summary>
+        /// <returns>string con todos los valores de la instancia de la clase</returns>
+        public override string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("LECHE");
+            sb.AppendLine((string)this);
+            sb.Append(string.Format("CALORIAS : {0}", this.CantidadCalorias));
+            sb.AppendLine("TIPO : " + this.tipo);
+            sb.AppendLine("");
+            sb.AppendLine("----------------------");
+
+            return sb.ToString();
+        }
+        #endregion
+    }
+}
---
+++ RecuperatoriosTP/TP2/TP-02/Entidades/Producto.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Text;
+
+namespace Entidades_2018
+{
+    /// <summary>
+    /// La clase Producto no deberá permitir que se instancien elementos de este tipo.
+    /// </summary>
+    public abstract class Producto
+    {
+        #region "Atributos"
+        public enum EMarca
+        {
+            Serenisima, Campagnola, Arcor, Ilolay, Sancor, Pepsico
+        }
+        private EMarca marca;
+        private string codigoDeBarras;
+        private ConsoleColor colorPrimarioEmpaque;
+        #endregion
+
+        #region "Propiedades"
+        /// <summary>
+        /// ReadOnly: Retornará la cantidad de calorías del producto
+        /// </summary>
+        protected abstract short CantidadCalorias { get; }
+        #endregion
+
+        #region "Constructores"
+        public Producto(string patente, EMarca marca, ConsoleColor color)
+        {
+            this.marca = marca;
+            this.codigoDeBarras = patente;
+            this.colorPrimarioEmpaque = color;
+        }
+        #endregion
+
+        #region "Métodos"
+        /// <summary>
+        /// Publica todos los datos del Producto.
+        /// </summary>
+        /// <returns></returns>
+        public abstract string Mostrar();
+        #endregion
+
+        #region "Operadores"
+        /// <summary>
+        /// Recibe un Producto el cual debe castear a string 
+        /// </summary>
+        /// <param name="p"></param>
+        /// <returns>todos sus atributos</returns>
+        public static explicit operator string(Producto p)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine(string.Format("CODIGO DE BARRAS: {0}", p.codigoDeBarras));
+            sb.AppendLine(string.Format("MARCA          : {0}", p.marca.ToString()));
+            sb.AppendLine(string.Format("COLOR EMPAQUE  : {0}", p.colorPrimarioEmpaque.ToString()));
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// Dos productos son iguales si comparten el mismo código de barras
+        /// </summary>
+        /// <param name="v1"></param>
+        /// <param name="v2"></param>
+        /// <returns></returns>
+        public static bool operator ==(Producto v1, Producto v2)
+        {
+            return (v1.codigoDeBarras == v2.codigoDeBarras);
+        }
+        /// <summary>
+        /// Dos productos son distintos si su código de barras es distinto
+        /// </summary>
+        /// <param name="v1"></param>
+        /// <param name="v2"></param>
+        /// <returns></returns>
+        public static bool operator !=(Producto v1, Producto v2)
+        {
+            return !(v1 == v2);
+        }
+        #endregion
+    }
+}
